60430a710552297d0e06e9afe6e66d068b283ef2
anshjainnnnn <166632616+anshjainnnnn@users.noreply.github.com>
2024-04-27 19:32:38 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public class Addition {
+        public static void main(String[] args) {
+            int num1 = 10;
+            int num2 = 20;
+            
+            int sum = num1 + num2;
+            
+            System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        }
+    }
+    
+}
